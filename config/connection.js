3ee84f01475ad7850bab7b998577a54e2204b271
Kevin Whiteside 1 <kevindwhiteside@icloud.com>
2016-10-22 09:37:23 -0400

    heroku jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'players_db'
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+		var connection = mysql.createConnection({
+	    host: 'localhost',
+	    user: 'root',
+	    password: '',
+	    database: 'players_db'
+	});
+}
+
 
 
 connection.connect(function(err) {
